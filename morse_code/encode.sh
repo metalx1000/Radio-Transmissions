024eeaf369db7a748b243c88af47c186019ac01c
metalx1000 <git@filmsbykris.com>
2017-11-14 19:04:36 -0500

    added notes on real time msg sending

--- morse_code/encode.sh
+++ morse_code/encode.sh
@@ -3,3 +3,9 @@
 echo "running command:"
 echo "cat data.log|morse"
 cat data.log|morse
+
+
+echo -e "\n\n\nYou can also send messages as you type."
+echo "mkfifo mcode"
+echo "cat mcode|morse"
+echo "cat > mcode"
